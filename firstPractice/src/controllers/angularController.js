d35d3bda1010f01e4a5efc37acbcf7bd5fa595bf
JMStudiosJoe <jrichardson@jmstudios.net>
2016-05-22 08:24:26 -0700

    cleaned code

--- firstPractice/src/controllers/angularController.js
+++ firstPractice/src/controllers/angularController.js
@@ -1,10 +1,10 @@
 
 
 app.controller('firstController', ['$scope', function($scope){
-	console.log("yes this is auto refreshing");
+
 	$scope.hello = "joey";
-	$scope.wat = "why";
-	console.log($scope.hello);
+
+
 }]);
 app.config(function($logProvider){
     $logProvider.debugEnabled(true);
